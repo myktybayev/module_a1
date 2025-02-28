d75bdf42e22c9c4be54daa229d895be9f37d6dc8
myktybayev <mykty-baha@bk.ru>
2025-02-28 11:33:19 +0500

    json added

--- app/src/main/java/kz/worldskills/a107/ui/authen/LoginActivity.java
+++ app/src/main/java/kz/worldskills/a107/ui/authen/LoginActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import kz.worldskills.a107.MainActivity;
 import kz.worldskills.a107.R;
@@ -25,13 +30,14 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnLogin);
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
-
+        
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String email = editTextEmail.getText().toString();
+                String pass = editTextPassword.getText().toString();
 
-                if(email.length()==0 || editTextPassword.getText().length()==0 ){
+                if(email.length()==0 || pass.length()==0 ){
                     Toast.makeText(LoginActivity.this, "Заполните все поля!", Toast.LENGTH_SHORT).show();
                 }
 
@@ -40,8 +46,9 @@ public class LoginActivity extends AppCompatActivity {
                 }
 
                 else{
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
+
+                    checkJSONAccount(email, pass);
+                    
                 }
 
             }
@@ -57,5 +64,39 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
+    }
+    
+    public void checkJSONAccount(String uEmail, String uPassword){
+        String jsonString = LoadJSONAsset.loadJSONFromAsset(this, "login.json");
+        if (jsonString != null) {
+            try {
+                JSONArray jsonArray = new JSONArray(jsonString);
+                boolean loginTabildi = false;
+                
+                for(int i = 0; i < jsonArray.length(); i++){
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    String email = jsonObject.getString("mail");
+                    String password = jsonObject.getString("password");
+                    
+                    if(email.equals(uEmail) && password.equals(uPassword)){
+                        loginTabildi = true;
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                }
+
+                if(loginTabildi == false){
+                    Toast.makeText(this, "Not found user from JSON file", Toast.LENGTH_SHORT).show();
+                }
+                
+                
+
+            } catch (JSONException e) {
+
+                Log.i("json_text", "e: "+e.toString());
+                e.printStackTrace();
+            }
+        }
     }
 }
--- app/src/main/java/kz/worldskills/a107/ui/home/CatalogFragment.java
+++ app/src/main/java/kz/worldskills/a107/ui/home/CatalogFragment.java
@@ -1,9 +1,12 @@
 package kz.worldskills.a107.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.SearchView;
@@ -12,10 +15,17 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import kz.worldskills.a107.MainActivity;
 import kz.worldskills.a107.R;
+import kz.worldskills.a107.ui.authen.LoadJSONAsset;
+import kz.worldskills.a107.ui.authen.LoginActivity;
 
 public class CatalogFragment extends Fragment {
 
@@ -31,14 +41,39 @@ public class CatalogFragment extends Fragment {
         searchView = view.findViewById(R.id.searchView);
         itemList = new ArrayList<>();
 
-        itemList.add(new Item(R.drawable.ic_notifications_black_24dp, "Laptop", "Ноутбук описание", 200000, 0));
-        itemList.add(new Item(R.drawable.ic_catalog, "MacBook", "Apple Macbook", 1800000, 0));
-        itemList.add(new Item(R.drawable.ic_catalog, "Phone", "Телефон описание", 300000, 0));
-        itemList.add(new Item(R.drawable.ic_account, "Car", "Машина описание", 240000, 0));
-        itemList.add(new Item(R.drawable.ic_korzinka, "Car1", "Мышка описание", 250000, 0));
-        itemList.add(new Item(R.drawable.ic_korzinka, "Мышка2", "Мышка описание", 150000, 0));
-        itemList.add(new Item(R.drawable.ic_korzinka, "Мышка3", "Мышка описание", 100000, 0));
-        itemList.add(new Item(R.drawable.ic_korzinka, "Мышка", "Мышка описание", 12000, 0));
+        String jsonString = LoadJSONAsset.loadJSONFromAsset(getActivity(), "store.json");
+        if (jsonString != null) {
+            try {
+                JSONArray jsonArray = new JSONArray(jsonString);
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    String title = jsonObject.getString("name");
+                    String desc = jsonObject.getString("desc");
+                    int baga = jsonObject.getInt("price");
+
+                    itemList.add(new Item(R.drawable.ic_notifications_black_24dp, title, desc, baga, 0));
+
+
+                }
+
+
+            } catch (JSONException e) {
+
+                Log.i("json_text", "e: " + e.toString());
+                e.printStackTrace();
+            }
+        }
+
+//        itemList.add(new Item(R.drawable.ic_notifications_black_24dp, "Laptop", "Ноутбук описание", 200000, 0));
+//        itemList.add(new Item(R.drawable.ic_catalog, "MacBook", "Apple Macbook", 1800000, 0));
+//        itemList.add(new Item(R.drawable.ic_catalog, "Phone", "Телефон описание", 300000, 0));
+//        itemList.add(new Item(R.drawable.ic_account, "Car", "Машина описание", 240000, 0));
+//        itemList.add(new Item(R.drawable.ic_korzinka, "Car1", "Мышка описание", 250000, 0));
+//        itemList.add(new Item(R.drawable.ic_korzinka, "Мышка2", "Мышка описание", 150000, 0));
+//        itemList.add(new Item(R.drawable.ic_korzinka, "Мышка3", "Мышка описание", 100000, 0));
+//        itemList.add(new Item(R.drawable.ic_korzinka, "Мышка", "Мышка описание", 12000, 0));
 
         catalogAdapter = new CatalogAdapter(getActivity(), itemList);
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
